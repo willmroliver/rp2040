db8ab67e32f8cd288c2f41b43b2343bf1d4ee553
Will Oliver <willoliver.main@gmail.com>
2025-07-10 16:31:18 +0100

    improved references to memory regions per ARM learning resources

--- blink.c
+++ blink.c
@@ -1,21 +1,22 @@
 #include <stdint.h>
 
 #define SIO_BASE 0xd0000000
-#define GPIO_OE 0x020
-#define GPIO_OE_SET 0x024
-#define GPIO_OE_CLR 0x028
-#define GPIO_IN 0x004
-#define GPIO_OUT 0x010
-#define GPIO_OUT_SET 0x014
-#define GPIO_OUT_CLR 0x018
-#define GPIO_OUT_XOR 0x01c
+#define GPIO_IN (*((volatile unsigned long *)(SIO_BASE + 0x004)))
+#define GPIO_OUT (*((volatile unsigned long *)(SIO_BASE + 0x010)))
+#define GPIO_OUT_SET (*((volatile unsigned long *)(SIO_BASE + 0x014)))
+#define GPIO_OUT_CLR (*((volatile unsigned long *)(SIO_BASE + 0x018)))
+#define GPIO_OUT_XOR (*((volatile unsigned long *)(SIO_BASE + 0x01c)))
+#define GPIO_OE (*((volatile unsigned long *)(SIO_BASE + 0x020)))
+#define GPIO_OE_SET (*((volatile unsigned long *)(SIO_BASE + 0x024)))
+#define GPIO_OE_CLR (*((volatile unsigned long *)(SIO_BASE + 0x028)))
 
 #define IO_BANK0_BASE 0x40014000
-#define GPIO_FUNCSEL_SIO 0x5
+#define GPIO_CTRL(io) (*((volatile unsigned long *)(IO_BANK0_BASE + 8*io + 4)))
+#define GPIO_FUNCSEL_SIO 0x5 
 
 #define RESETS_BASE 0x4000c000
-#define RESETS_RESET 0x0
-#define RESETS_DONE 0x8
+#define RESETS_RESET (*((volatile unsigned long *)(RESETS_BASE + 0x0)))
+#define RESETS_DONE (*((volatile unsigned long *)(RESETS_BASE + 0x8)))
 
 typedef unsigned int u32;
 typedef char u8;
@@ -43,7 +44,7 @@ void blink()
 	const u8 IO_BANK0 = 5;
 	const u8 LED = 25;
 
-	volatile uint64_t freq = 250000, end;
+	volatile uint64_t delay_us = 250000, end;
 
 	init_io((1 << IO_BANK0));
 	funcsel(LED, GPIO_FUNCSEL_SIO);
@@ -52,26 +53,23 @@ void blink()
 
 	while (1) {
 		gpio_put(LED, 1);
-		end = get_time_us() + freq;
+		end = get_time_us() + delay_us;
 		while (get_time_us() < end);
 		gpio_put(LED, 0);
-		end = get_time_us() + freq;
+		end = get_time_us() + delay_us;
 		while (get_time_us() < end);
 	}
 }
 
 void init_io(u32 mask)
 {
-	u32 *set = reset_reg_get(RESETS_RESET);
-	u32 *test = reset_reg_get(RESETS_DONE);
-
-	*set &= ~mask;
-	while (!(*test & mask));
+	RESETS_RESET &= ~mask;
+	while (!(RESETS_DONE & mask));
 }
 
 void init_gpio(u8 io)
 { 
-	*sio_reg_get(GPIO_OE_SET) = (1 << io);
+	GPIO_OE_SET = (1 << io);
 }
 
 u32 *reg_get(u32 base, u32 offset)
@@ -79,35 +77,16 @@ u32 *reg_get(u32 base, u32 offset)
 	return (u32*)(base + offset);
 }
 
-u32 *sio_reg_get(u32 offset)
-{
-	return (u32*)(SIO_BASE + offset);
-}
-
-u32 *reset_reg_get(u32 offset)
-{
-	return (u32*)(RESETS_BASE + offset);
-}
-
-u32 *gpio_ctrl(u8 io)
-{
-	return (u32*)(IO_BANK0_BASE + (u32)io * 8 + 4);
-}
-
 void funcsel(u8 io, u32 fn) 
 {
-	*gpio_ctrl(io) = fn;
+	GPIO_CTRL(io) = fn;
 }
 
 void gpio_put(u8 io, u8 val)
 {
-	u32 *sio_reg;
-
 	if (val) {
-		sio_reg = sio_reg_get(GPIO_OUT_SET);
+		GPIO_OUT_SET = (1 << io);
 	} else {
-		sio_reg = sio_reg_get(GPIO_OUT_CLR);
+		GPIO_OUT_CLR = (1 << io);
 	}
-
-	*sio_reg = (1 << io);
 }
